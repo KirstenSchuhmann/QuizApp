e2414c0ec8ddff7cca5020c2cc99550052336b5c
Kirsten Schuhmann <kirsten.schuhmann@hotmail.de>
2022-11-04 19:21:41 +0100

    java beginn

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+const question = document.querySelector('[data-js="question"');
+const answerBtn = document.querySelector(
+  '[data-js="quiz-card--answer-button"]'
+);
+const answer = document.querySelector('[data-js="answer"]');
+const tags = document.querySelector('[data-js="quiz-card__tags"]');
+
+
+answer.addEventListener('click' () => {
+   console.log(answer); 
+
+});
