5f008849ac97fda434c82f221a01465430abaf27
Kirsten Schuhmann <kirsten.schuhmann@hotmail.de>
2022-11-05 20:58:31 +0100

    Added Darkmode Colors

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,8 @@
 const body = document.querySelector("body");
+
 const question = document.querySelector('[data-js="question"');
 const answerBtn = document.querySelector(
   '[data-js="quiz-card--answer-button"]'
 );
 const answer = document.querySelector('[data-js="answer"]');
 const tags = document.querySelector('[data-js="quiz-card__tags"]');
-
-answerBtn.addEventListener("click", () => {
-  answer.classList.add("addedClassViaJS");
-});
-
-/*
-answer.addEventListener('click', () => {
-   if (answer.classList.contains("quiz-card__answer--hidden")) {
-    answer.setAttribute.
-   }
-});
-*/
