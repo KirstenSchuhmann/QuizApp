4cc02c36d655f6ea52e3c6f74e35c043c73b2539
Kirsten Schuhmann <kirsten.schuhmann@hotmail.de>
2022-11-06 12:16:55 +0100

    added icons for darkmode to profile page

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,33 @@ const answerBtn = document.querySelector(
 );
 const answer = document.querySelector('[data-js="answer"]');
 const tags = document.querySelector('[data-js="quiz-card__tags"]');
+
+const sun = document.querySelector('[data-js="sun]');
+
+sun.addEventListener("click", function () {
+  sun.target.classList.toggle("moon"); 
+  if (sun.classList.toggle("sun")) {
+    body.classList.add('body');
+  }
+  else if (sun.classList.add("sun")) {
+    body.classList.add('body');
+  }
+  
+  
+});
+
+});
+
+/* darkmode.addEventListener("click", function () {
+  this.classList.toggle("moon");
+  if (this.classList.toggle("sun")) {
+    body.style.background = "white";
+    body.style.color = "black";
+    body.style.transition = "2s";
+  } else {
+    body.style.background = "black";
+    body.style.color = "white";
+    body.style.transition = "2s";
+  }
+});
+*/
