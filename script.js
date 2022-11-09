73f992eff43e3b6d7e96468b55956d8edc2ec34e
Kirsten Schuhmann <kirsten.schuhmann@hotmail.de>
2022-11-09 14:06:15 +0100

    changes

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ quiz-card => .quiz-card--darkmode
 const quizCard = document.querySelector('[data-js="quiz-card"]');
 const bookmark = document.querySelector('[data-js="bookmark"]');
 
-// Darkmode Function (Noch nicht vervollständigt!)
+// Darkmode Function
 const Sun = document.querySelector('[data-js="sun"]');
+const Cat = document.querySelector('[data-js="Cat"');
 const btnSun = document.querySelector('[data-js="btn-sun"]');
 const body = document.querySelector('[data-js="body"]');
 
-/*btnSun.addEventListener("click", () => {
-  body.classList.toggle("darkmode");
-});*/
-
 btnSun.addEventListener("click", () => {
-  if(body.classList.contains('darkmode') == 'true')
+  body.classList.toggle("darkmode");
+  Sun.classList.toggle("moon");
+  quizCard.classList.toggle("quiz-card--darkmode");
+  bookmark.classList.toggle("quiz-card__bookmark--darkmode");
+  Cat.classList.toggle("nightCat");
 });
